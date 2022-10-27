4bce0bc559d3af7dc9762ce5189c05b7e97bab33
YhinkaDevOps <yinkabakare30@gnail.com>
2022-10-27 20:26:16 +0200

    create main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='Main'>
+     <div className='main-section'></div>
+     <div className='social-section'></div>
+    </div>
+  )
+}
+
+export default Main
