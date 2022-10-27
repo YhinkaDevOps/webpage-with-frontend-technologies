1b734d7a4fca58ac4d6d543a848180704cbc32bb
YhinkaDevOps <yinkabakare30@gnail.com>
2022-10-27 23:00:21 +0200

    create twitter container and image tag

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,13 @@ const Main = () => {
      <div className='main-section'>
       <div className='main-content'>
         <div className='profile-section' >
-          <img src="" alt="profile-img" />
+          <img src="" alt='profile-img' id='profile-img' />
+          <div id='twitter'>
+            <p className=' text-gray-900'>
+              Mahoetey
+            </p>
+
+          </div>
         </div>
         <div className='link-section'></div>
       </div>
