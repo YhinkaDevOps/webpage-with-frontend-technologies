dc6b2d652cb22b1191187dd9b3f88c6fe7011593
YhinkaDevOps <yinkabakare30@gnail.com>
2022-10-29 00:28:15 +0200

    clean main component
    Deleted redundant tailwind class utilities

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
+import React from "react"
 
 const Main = () => {
   return (
-    <div className='main-container-'>
-     <div className='main-section'>
-      <div className='main-content'>
-        <div className='profile-section' >
-          <img src="" alt='profile-img' id='profile-img' />
-          <div id='twitter'>
-            <p className=' text-gray-900'>
-              Mahoetey
-            </p>
-
+    <div className="main-container pt-16 pl-28 pr-28 ">
+      <div className="main-section pr-8 pl-8 items-center flex flex-col ">
+        <div className="main-content flex flex-col items-start p-0">
+          <div className="profile-section"></div>
+          <div className="link-section"></div>
+        </div>
+      </div>
+      <div className="social-section mt-6 pt-6 pb-6">
+        <div className="social-container pt-6  pb-6">
+          <div className="social-content flex flex-row items-center">
+            <div className="social-icons flex flex-row items-center justify-center "></div>
           </div>
         </div>
-        <div className='link-section'></div>
       </div>
-     </div>
-     <div className='social-section'>
-
-     </div>
     </div>
   )
 }
