8ca2b7f8994eb226ed06da781a87498291305092
YhinkaDevOps <yinkabakare30@gnail.com>
2022-10-29 02:38:51 +0200

    import profile image
    Imported profile image and styled it including twitter name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,22 @@
 import React from "react"
+import profileimage from "../assets/images/profile__img.png"
 
 const Main = () => {
   return (
     <div className="main-container pt-16 pl-28 pr-28 ">
       <div className="main-section pr-8 pl-8 items-center flex flex-col ">
         <div className="main-content flex flex-col items-start p-0">
-          <div className="profile-section"></div>
+          <div className="profile-section flex flex-col items-center">
+            <img
+              className="mb-6"
+              src={profileimage}
+              alt="profile-image"
+              id="profile__img"
+            />
+            <p className="font-inter" id="twitter">
+              Mahoety
+            </p>
+          </div>
           <div className="link-section"></div>
         </div>
       </div>
