f9f53212b1099fdc2008c386ad968d3088c2388f
YhinkaDevOps <yinkabakare30@gnail.com>
2022-11-02 07:11:07 +0200

    final changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import I4G from '../assets/svg/I4G.svg';
 
 const Footer = () => {
   return (
-    <div className="footer h-[196px] mb-16 md:h-40 pt-8 pb-8 md:pt-12 md:pb-12 md:pl-20 md:pr-20">
+    <div className="footer h-[196px] md:h-40 pt-8 pb-8 md:pt-12 md:pb-12 md:pl-20 md:pr-20">
       <div className="footer-container pt-12 pb-12 pl-4 pr-4 md:pl-8 md:pr-8">
         <div className="footer-content flex flex-col items-start md:flex-row md:justify-between md:items-center h-16 pt-8 gap-y-4 ">
           <img src={Zurilogo} alt="zuri-logo" />
